8b6d0f2c2482ee1210c2437bf188e8375286bf1e
Luigi9411 <luigi.sciavicco@gmail.com>
2022-11-12 14:02:02 +0100

    add-button-bonus

--- js/script.js
+++ js/script.js
@@ -1,39 +1,38 @@
-// Esercizio di oggi: Carosello Array di Oggetti
-// nome repo: js-array-objects-carousel
-// Consegna:
-// Riprendiamo l'esercizio carosello e modifichiamone il codice per renderlo funzionante con un array di oggetti al posto dell'array semplice.
-// Ecco l'array con i dati:
-// const images = [
-// 	{
-// 		image: '01.webp',
-// 		title: "Marvel's Spiderman Miles Morale",
-// 		text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-// 	},
-// 	{
-// 		image: '02.webp',
-// 		title: 'Ratchet & Clank: Rift Apart',
-// 		text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-// 	},
-// 	{
-// 		image: '03.webp',
-// 		title: 'Fortnite',
-// 		text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
-// 	},
-// 	{
-// 		image: '04.webp',
-// 		title: 'Stray',
-// 		text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-// 	},
-// 	{
-// 		image: '05.webp',
-// 		title: "Marvel's Avengers",
-// 		text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-// 	},
-// ];
-// BONUS 1:
-// Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.
-// BONUS 2:
-// Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
+function moveDown() {
+	 
+	listEleImg[activeIndex].classList.remove('active');
+	listThumbs[activeIndex].classList.remove('active');
+	listEleText[activeIndex].classList.remove('active');
+	
+	activeIndex++;
+	if (activeIndex === listEleImg.length) {
+		activeIndex = 0;
+	}
+	
+	listEleImg[activeIndex].classList.add('active');
+	listThumbs[activeIndex].classList.add('active');
+	listEleText[activeIndex].classList.add('active');
+	;
+}
+
+function moveUp () {
+	 
+	listEleImg[activeIndex].classList.remove('active');
+	listThumbs[activeIndex].classList.remove('active');
+	listEleText[activeIndex].classList.remove('active');
+	
+	if (activeIndex === 0) {
+		activeIndex = listEleImg.length;
+	}
+	activeIndex--;
+
+	
+	listEleImg[activeIndex].classList.add('active');
+	listThumbs[activeIndex].classList.add('active');
+	listEleText[activeIndex].classList.add('active');
+	
+}
+
 const arrImages = [
     {
      	image: '01.webp',
@@ -62,6 +61,9 @@ const arrImages = [
     },
  ];
 	
+ const timeSlider = 1.5*1000;
+ let direction = 1;
+ let isAutoplayActive = true;
 
  const eleSlider = document.querySelector('.slider-viewer');
  const eleButtonUp = document.querySelector('.button-up');
@@ -99,7 +101,34 @@ const arrImages = [
 	 eleSliderThumbs.append(eleThumb);
  }
  
- 
+ let idInterval =setInterval(() => {
+	if (direction > 0) {
+		moveDown();
+	} else {
+		moveUp()
+	}
+ },timeSlider);
+
+ document.querySelector('.btn-invert').addEventListener('click',() => direction*= -1);
+
+ document.querySelector('.btn-start-stop').addEventListener('click',function() {
+	if (this.dataset.functionality === 'stop'){
+		clearInterval(idInterval);
+		this.innerHTML='Start';
+		this.dataset.functionality = 'start';
+	} else {
+		 idInterval =setInterval(() => {
+			if (direction > 0) {
+				moveDown();
+			} else {
+				moveUp()
+			}
+		 },timeSlider);
+		this.innerHTML='Stop';
+		this.dataset.functionality = 'stop';
+	}
+ } );
+
  const listEleImg = document.querySelectorAll('.slider-img'); 
  const listThumbs = document.querySelectorAll('.thumb-img');
  const listEleText = document.querySelectorAll('.text')
@@ -108,37 +137,7 @@ const arrImages = [
  
  
  
- eleButtonDown.addEventListener('click', function () {
-	 
-	 listEleImg[activeIndex].classList.remove('active');
-	 listThumbs[activeIndex].classList.remove('active');
-	 listEleText[activeIndex].classList.remove('active');
-	 
-	 activeIndex++;
-	 if (activeIndex === listEleImg.length) {
-		 activeIndex = 0;
-	 }
-	 
-	 listEleImg[activeIndex].classList.add('active');
-	 listThumbs[activeIndex].classList.add('active');
-	 listEleText[activeIndex].classList.add('active');
-	 ;
- });
+ eleButtonDown.addEventListener('click', moveDown);
  
- eleButtonUp.addEventListener('click', function () {
-	 
-	 listEleImg[activeIndex].classList.remove('active');
-	 listThumbs[activeIndex].classList.remove('active');
-	 listEleText[activeIndex].classList.remove('active');
-	 
-	 if (activeIndex === 0) {
-		 activeIndex = listEleImg.length;
-	 }
-	 activeIndex--;
- 
-	 
-	 listEleImg[activeIndex].classList.add('active');
-	 listThumbs[activeIndex].classList.add('active');
-	 listEleText[activeIndex].classList.add('active');
-	 
- });
+ eleButtonUp.addEventListener('click', moveUp );
+
